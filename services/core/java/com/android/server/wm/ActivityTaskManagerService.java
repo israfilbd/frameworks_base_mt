73e9f41a234aa887071cc5cd3bbac3b64f2b1f42
Alvin Francis <nivlafx@gmail.com>
2024-06-02 21:18:40 -0400

    PixelPropsUtils: Final cleanup
     * Lets build on top of this testing for other devices
     * Fixed editing functions and other pixel features not working due to spoofing, they all work spoofed or not spoofed on cheetah.
     * Includes fixes from the respected authors below
     * Tested on motorola racer, nga and other pixel related features work
    
    PropImitationHooks: Un-spoof GMS while adding google account
    
    GMS uses its DroidGuard service (the same that's used for safetynet)
    to get the device name from the build fingerprint, which from our
    current certified props, is Pixel XL. The device name after Google
    login shows up in the login alert accordingly as Pixel XL, which is
    inconvenient especially when you have multiple devices on the same ROM.
    
    By restarting the DroidGuard service process (gms.unstable) as soon as
    the user starts adding Google account (i.e when the GMS add account
    activity is started and moved to the top of the task stack), we can
    skip spoofing to certified props in this instance. When the user
    stops/finishes adding account (GMS add account activity is no longer
    at the top of the task stack), restart the process again, this time
    spoofing to certified props in order to be ready for safetynet.
    
    This ensures while adding a Google account, GMS reads the original
    properties of the device and labels the device with its original name,
    while in any other situation GMS reads certified properties required
    to pass SafetyNet/Play Integrity.
    
    Test:
    - Add google account from setup wizard, settings, any other app
    - Observe correct device name in gmail login alert
    - SafetyNet (basic, CTS), Play Integrity (basic, device) passes.
    
    Change-Id: Ifc9bd4d4295cfb7c0eaad6172e12f88c22d6ec26
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    
    PixelPropsUtils: shouldTryToCertifyDevice
    
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    PPU: Allow spoofing INITIAL_SDK_INT
    
    Co-authored-by: AnierinB <anierin@evolution-x.org>
    
    PixelPropsUtils: Only spoof unstable process
    
    * That's the only process we needed
    
    Change-Id: I40423585c439cb0e55e50bad3d1a2b54871ae5eb
    Co-authored-by: someone5678 <someone5678@users.noreply.github.com>
    
    Signed-off-by: Alvin Francis <nivlafx@gmail.com>

--- services/core/java/com/android/server/wm/ActivityTaskManagerService.java
+++ services/core/java/com/android/server/wm/ActivityTaskManagerService.java
@@ -2008,7 +2008,7 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
 
     @Override
     public RootTaskInfo getFocusedRootTaskInfo() throws RemoteException {
-        enforceTaskPermission("getFocusedRootTaskInfo()");
+            enforceTaskPermission("getFocusedRootTaskInfo()");
         final long ident = Binder.clearCallingIdentity();
         try {
             synchronized (mGlobalLock) {
@@ -3130,7 +3130,7 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
     /** Sets the task stack listener that gets callbacks when a task stack changes. */
     @Override
     public void registerTaskStackListener(ITaskStackListener listener) {
-        enforceTaskPermission("registerTaskStackListener()");
+            enforceTaskPermission("registerTaskStackListener()");
         mTaskChangeNotificationController.registerTaskStackListener(listener);
     }
 

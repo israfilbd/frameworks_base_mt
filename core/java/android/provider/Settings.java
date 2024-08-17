4b35e1177ccba8ab01af2e41310b106a1eed7625
Aston-Martinn <advaithbhat9@gmail.com>
2024-08-17 17:30:59 +0530

    SystemUI: Ambient display show battery & cpu temperature [1/2]
    Change-Id: I680cb9057577fa6ae401a5a72b3ae3f7378a9861
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7318,6 +7318,16 @@ public final class Settings {
         public static final String ARCANE_IDLE_MANAGER = "arcane_idle_manager";
 
         /**
+         * What to show at the bottom of the Ambient display
+         * 0: Nothing.
+         * 1: Battery Percentage.
+         * 2: Battery Temperature.
+         * 3: Battery Percentage & Temperature Together.
+         * @hide
+         */
+        public static final String AMBIENT_SHOW_SETTINGS = "ambient_show_settings";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -1134,7 +1134,7 @@ public class KeyguardIndicationController {
             boolean useMisalignmentColor = false;
             mLockScreenIndicationView.setVisibility(View.GONE);
             mTopIndicationView.setVisibility(VISIBLE);
-            CharSequence newIndication;
+            CharSequence newIndication = "";
             boolean setWakelock = false;
 
             if (!TextUtils.isEmpty(mBiometricMessage)) {
@@ -1156,8 +1156,24 @@ public class KeyguardIndicationController {
                 newIndication = computePowerIndication();
                 setWakelock = animate;
             } else {
-                newIndication = NumberFormat.getPercentInstance()
-                        .format(mBatteryLevel / 100f);
+                String batteryTemp = com.android.internal.util.crdroid.Utils.batteryTemperature(mContext, false);
+                String cpuTemp = com.android.internal.util.crdroid.Utils.batteryTemperature(mContext, false);
+                switch (getAmbientShowSettings()) {
+                    case 0: // Show battery level
+                        newIndication = NumberFormat.getPercentInstance()
+                                .format(mBatteryLevel / 100f);
+                        break;
+                    case 1: // Battery level & battery temperature
+                        newIndication = NumberFormat.getPercentInstance()
+                                .format(mBatteryLevel / 100f) +
+                                " | " + batteryTemp;
+                        break;
+                    case 2: // Battery level, battery temperature & cpu temperature
+                        newIndication = NumberFormat.getPercentInstance()
+                                .format(mBatteryLevel / 100f) +
+                                " | " + batteryTemp + " | " + cpuTemp;
+                        break;
+                }
                 setWakelock = false;
             }
 
@@ -1192,6 +1208,11 @@ public class KeyguardIndicationController {
         mTopIndicationView.setText(null);
         mLockScreenIndicationView.setVisibility(View.VISIBLE);
         updateLockScreenIndications(animate, getCurrentUser());
+    }
+
+    private int getAmbientShowSettings() {
+        return Settings.System.getIntForUser(mContext.getContentResolver(),
+            Settings.System.AMBIENT_SHOW_SETTINGS, 0, UserHandle.USER_CURRENT);
     }
 
     /**

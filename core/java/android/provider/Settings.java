e29dc34d24765438aa6850cf8f4b06e72eaf2dda
Aston-Martinn <advaithbhat9@gmail.com>
2024-08-18 13:50:57 +0530

    SystemUI: Show icons along with indication in doze [1/2]
    Change-Id: Ibdb5a0bfd3e508c34e4ecbf726f88274fa875255
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7328,6 +7328,12 @@ public final class Settings {
         public static final String AMBIENT_SHOW_SETTINGS = "ambient_show_settings";
 
         /**
+         * Ambient settings show icons
+         * @hide
+         */
+        public static final String AMBIENT_SHOW_SETTINGS_ICONS = "ambient_show_settings_icons";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *

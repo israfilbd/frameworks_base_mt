bbfb18551357835eb5c8c7e98288e26e87596302
drkphnx <dark.phnx12@gmail.com>
2024-12-12 15:42:12 +0800

    core: Add transperent notification style [EXPERIMENTAL] [1/3]
    * looks better with two tone style
    
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Co-authored-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: darkphnx <dark.phnx12@gmail.com>

--- packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayApplier.java
+++ packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayApplier.java
@@ -135,7 +135,9 @@ public class ThemeOverlayApplier implements Dumpable {
     @VisibleForTesting
     static final String OVERLAY_CATEGORY_LOCK_CLOCK_FONT =
             "android.theme.customization.lockscreen_clock_font";
-
+    @VisibleForTesting
+    static final String OVERLAY_CATEGORY_NOTIFICATION =
+           "android.theme.customization.notification";
     /*
      * All theme customization categories used by the system, in order that they should be applied,
      * starts with launcher and grouped by target package.
@@ -152,7 +154,8 @@ public class ThemeOverlayApplier implements Dumpable {
             OVERLAY_CATEGORY_ICON_SIGNAL,
             OVERLAY_CATEGORY_ICON_WIFI,
             OVERLAY_CATEGORY_NAVBAR,
-            OVERLAY_CATEGORY_LOCK_CLOCK_FONT);
+            OVERLAY_CATEGORY_LOCK_CLOCK_FONT,
+            OVERLAY_CATEGORY_NOTIFICATION);
 
     /* Categories that need to be applied to the current user as well as the system user. */
     @VisibleForTesting
@@ -165,7 +168,8 @@ public class ThemeOverlayApplier implements Dumpable {
             OVERLAY_CATEGORY_ICON_ANDROID,
             OVERLAY_CATEGORY_ICON_SYSUI,
             OVERLAY_CATEGORY_NAVBAR,
-            OVERLAY_CATEGORY_LOCK_CLOCK_FONT);
+            OVERLAY_CATEGORY_LOCK_CLOCK_FONT,
+            OVERLAY_CATEGORY_NOTIFICATION);
 
     /* Allowed overlay categories for each target package. */
     private final Map<String, Set<String>> mTargetPackageToCategories = new ArrayMap<>();
@@ -209,6 +213,7 @@ public class ThemeOverlayApplier implements Dumpable {
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_ICON_WIFI, SYSUI_PACKAGE);
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_NAVBAR, SYSUI_PACKAGE);
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_LOCK_CLOCK_FONT, ANDROID_PACKAGE);
+        mCategoryToTargetPackage.put(OVERLAY_CATEGORY_NOTIFICATION, SYSUI_PACKAGE);
 
         dumpManager.registerDumpable(TAG, this);
     }

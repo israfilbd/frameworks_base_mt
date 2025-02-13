9c4f7d9526b5338ac4b6b95a80c12efa1b9cbb92
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-02-13 21:41:00 +0800

    SystemUI: Fix auto brightness icon UI inconsistencies
    Change-Id: I9070872631055af996fb8a94e91e17d06aa420ac
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/settings/brightness/BrightnessController.java
+++ packages/SystemUI/src/com/android/systemui/settings/brightness/BrightnessController.java
@@ -46,6 +46,8 @@ import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 
+import com.android.settingslib.Utils;
+
 import com.android.internal.display.BrightnessSynchronizer;
 import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
@@ -444,6 +446,12 @@ public class BrightnessController implements ToggleSlider.Listener, MirroredBrig
             mIcon.setBackgroundResource(mAutomatic ?
                     R.drawable.bg_qs_brightness_auto_on :
                     R.drawable.bg_qs_brightness_auto_off);
+            int color = Utils.getColorAttrDefaultColor(mContext, mAutomatic 
+                    ? android.R.attr.textColorPrimaryInverse 
+                    : android.R.attr.colorAccent);
+            mIcon.post(() -> {
+                mIcon.getDrawable().setTint(color);
+            });
         }
     }
 

aeea9330e2eb948524720693b7e68506d8432df6
Advaith Bhat <advaithbhat9@gmail.com>
2025-02-14 00:52:05 +0530

    base: [Utils] Add method to determine battery and CPU temp also Use a divider overlay for CPU temperature
    This is squash of commits
    base: [Utils] Add method to determine battery temp: https://github.com/TenX-OS/frameworks_base/commit/e6f89fe90abb2bd3847550369c98fdb94e821356
    base: [Utils] Add a method to determine CPU temperature : https://github.com/TenX-OS/frameworks_base/commit/c81dc9ddae48457ae5d7ebab61dac6bc15221c29
    base: [Utils] Use a divider overlay for CPU temperature: https://github.com/TenX-OS/frameworks_base/commit/e938ca8ba8a6dd5738537f9d31e3cc81dae7464a
    
    Co-authored-by: Alex Cruz <du.alexcruz@gmail.com>
    Signed-off-by: darkphnx <dark.phnx12@gmail.com>

--- core/java/com/android/internal/util/crdroid/Utils.java
+++ core/java/com/android/internal/util/crdroid/Utils.java
@@ -18,9 +18,11 @@ package com.android.internal.util.crdroid;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.os.BatteryManager;
 import android.content.pm.ResolveInfo;
 import android.os.PowerManager;
 import android.os.SystemClock;
@@ -28,6 +30,12 @@ import android.os.SystemProperties;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Utils {
 
@@ -98,4 +106,54 @@ public class Utils {
         }
         return needsNav;
     }
+
+    public static String batteryTemperature(Context context, Boolean ForC) {
+        Intent intent = context.registerReceiver(null, new IntentFilter(
+                Intent.ACTION_BATTERY_CHANGED));
+        float  temp = ((float) (intent != null ? intent.getIntExtra(
+                BatteryManager.EXTRA_TEMPERATURE, 0) : 0)) / 10;
+        // Round up to nearest number
+        int c = (int) ((temp) + 0.5f);
+        float n = temp + 0.5f;
+        // Use boolean to determine celsius or fahrenheit
+        return String.valueOf((n - c) % 2 == 0 ? (int) temp :
+                ForC ? c * 9/5 + 32 + "°F" :c + "°C");
+    }
+
+    public static String getCPUTemp(Context context) {
+        String value;
+        if (fileExists(context.getResources().getString(
+            com.android.internal.R.string.config_cpu_temp_path))) {
+               value = readOneLine(context.getResources().getString(
+                  com.android.internal.R.string.config_cpu_temp_path));
+        } else {
+            value = "Error";
+        }
+        int cpuTempMultiplier = context.getResources().getInteger(
+                com.android.internal.R.integer.config_sysCPUTempMultiplier);
+        return value == "Error" ? "N/A" : String.format("%s", Integer.parseInt(value) / cpuTempMultiplier) + "°C";
+    }
+    
+    public static boolean fileExists(String filename) {
+        if (filename == null) {
+            return false;
+        }
+        return new File(filename).exists();
+    }
+    public static String readOneLine(String fname) {
+        BufferedReader br;
+        String line = null;
+        try {
+            br = new BufferedReader(new FileReader(fname), 512);
+            try {
+                line = br.readLine();
+            } finally {
+                br.close();
+            }
+        } catch (Exception e) {
+            return null;
+        }
+        return line;
+    }
+
 }

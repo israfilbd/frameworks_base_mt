ba4364e91de788a995b4591db183a5b73a4cc450
drkphnx <dark.phnx12@gmail.com>
2025-03-05 10:44:11 +0000

    base: Extend OngoingProgressbar feature to be controlled from Settings
    Change-Id: I6ef2607bcd2b3364fd1c61a3d50b51478fe26558
    
    Co-authored-by: sajidshahriar72543 <sazidshahriar39@gmail.com>
    Signed-off-by: drkphnx <dark.phnx12@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7346,6 +7346,12 @@ public final class Settings {
         public static final String QS_HEADER_CLOCK_SIZE = "qs_header_clock_size";
 
         /**
+         * Whether to show ongoing progressbar in the notification shade
+         * @hide
+         */
+        public static final String CONFIG_ONGOING_PROGRESSBAR = "config_enableOngoingProgressBarChip";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
